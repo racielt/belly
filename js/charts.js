4ad8bf8d27ef65778c82c520593939ac1f9d726f
racielt <raciel.tavitas@gmail.com>
2021-12-11 19:57:43 -0500

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -88,11 +88,37 @@ function buildCharts(sample) {
     // 9. Create the layout for the bar chart. 
     var barLayout = {
       title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
     };
-    
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
-  });
+ 
+   // 1. Create the trace for the bubble chart.
+   var bubbleData = [
+   {
+     x: otu_ids,
+     y: sample_values,
+     text: otu_labels,
+     mode: "markers",
+     marker: {
+       size: sample_values,
+       color: otu_ids,
+       colorscale:"red"
+     }
+   }
+  ];
+
+  // 2. Create the layout for the bubble chart.
+  var bubbleLayout = {
+    title: "Bacteria Cultures Per Sample",
+    xaxis:{ title: "OTU ID"},
+    margin: {t: 40 },
+    hovermode: "closest"
+  };
+
+  // 3. Use Plotly to plot the data with the layout.
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+});
 }
 
 init();
