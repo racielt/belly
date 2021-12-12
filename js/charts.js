aa6e98ffa0fbae54e48a39dac53f64b82e9a8695
racielt <raciel.tavitas@gmail.com>
2021-12-12 00:02:38 -0500

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -119,22 +119,13 @@ function buildCharts(sample) {
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
 
-        // Create a variable that holds the samples array. 
-    var metadata = data.metadata;
-
-    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-    var filters = metadata.filter((sampleObj) => sampleObj.id == sample);  
-
-    // 2. Create a variable that holds the first sample in the metadata array.
-    var results = filters[0];
-
     // 3. Create a variable that holds the washing frequency.
-    var washFrequency = results.wfrequency;
+    var washFrequency = results.wfreq;
     
     // 4. Create the trace for the gauge chart.
-    var gaugeData = [
+    var gaugeData = 
       {
-      value: parseFloat(washFrequency),
+      value: wfreq,
       type: "indicator",
       mode: "gauge+number",
       gauge: {
@@ -148,8 +139,7 @@ function buildCharts(sample) {
           { range: [8, 10], color: "green" },
         ],
       },
-      },   
-    ];
+    };
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
